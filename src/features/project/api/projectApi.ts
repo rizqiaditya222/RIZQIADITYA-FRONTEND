0a62bbbe0778e9021a4a3d70c8dac1ef97097b84
Rizqi <rizqiaditya222@gmail.com>
2026-01-01 00:46:58 +0700

    fix(network): changing base url

--- src/features/project/api/projectApi.ts
+++ src/features/project/api/projectApi.ts
@@ -2,6 +2,6 @@ import axiosClient from '@/lib/axios/axiosClient'
 import { Project } from '../types/project'
 
 export const getProjectsApi = async (): Promise<Project[]> => {
-  const res = await axiosClient.get('/projects')
+  const res = await axiosClient.get(`${process.env.NEXT_PUBLIC_API_URL}/projects`)
   return res.data.data
 }
